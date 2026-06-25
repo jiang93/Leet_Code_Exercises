11e8fd52d5b696ef59bece00287c9de20baacf68
jiang93 <jianghy1993@hotmail.com>
2026-06-25 16:11:16 +0800

    Update LeetCode question 3 solution

--- 3. Longest Substring Without Repeating Characters(Ver2).py
+++ 3. Longest Substring Without Repeating Characters(Ver2).py
@@ -19,14 +19,18 @@ class Solution:
         # check for longest substring 
         for idx in range(string_length): # idx = 0, 1, 2, 3, 4 ...
             char = s[idx]
-            print(idx, char)
+            # print(idx, char)
+
             # if duplicated characters found in string 
             if char in seen:
-                if (len(s[start_idx:idx]) > max_length):
+                if (len(s[start_idx:idx]) > max_length): # replace substring with current substring if it is longer 
                     substring = s[start_idx:idx]
                     max_length = len(substring)
-                start_idx = idx
-            seen[char] = idx
+
+                if (seen[char] + 1 > start_idx): # if previous seen char location is after the current start_idx, update the start_idx
+                    start_idx = seen[char] + 1
+
+            seen[char] = idx # save character and number of occurence into key-pairs
         
         # if no duplicated characters found in string
         if (len(s[start_idx:idx+1]) > max_length):
@@ -38,9 +42,9 @@ class Solution:
         # return f'The answer is "{substring}", with the length of {max_length}.'
 
 s1 = Solution()
-s1.lengthOfLongestSubstring("pwawkew")
-s1.lengthOfLongestSubstring("dvdkew")
-#s1.lengthOfLongestSubstring("aabc")
+s1.lengthOfLongestSubstring("pwwkew")
+#s1.lengthOfLongestSubstring("dvdkew")
+s1.lengthOfLongestSubstring("abba")
 #s1.lengthOfLongestSubstring("abcdee")
 #s1.lengthOfLongestSubstring("adbecdefe")
 #s1.lengthOfLongestSubstring("au")
