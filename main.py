483e1959c9836fd8bc3004ab5fcdbddd6f8062c6
jiang93 <jianghy1993@hotmail.com>
2026-07-29 17:36:27 +0800

    updated python revision notes and main.py

--- main.py
+++ main.py
@@ -1,38 +1,26 @@
-# Insertion sort - treat the first element as the initial sorted element, and rest are unsorted elements
-mylist = [11, 5, 64, 34, 25, 22, 12, 42, 5, 50, 90, 100]
+# Radix sort
+mylist = [11, 64, 34, 25, 22, 12, 42, 5, 30, 5]
 
-# i = 1 (current_value=5, index=1), j=0(index=0), mylist[1] = 11, mylist[0]=5
-# mylist = [5, 11, 64, 34, 25, 22, 12, 42, 5, 50, 90, 100]
-# # i = 2 (current_value=64, index=2), j=1, j=0
-# # mylist = [5, 11, 64, 34, 25, 22, 12, 42, 5, 50, 90, 100]
-# # # i = 3 (current_value=34, index=3), j=2(index=2), mylist[3] = 64, mylist[2] = 34
-# # # mylist = [5, 11, 34, 64, 25, 22, 12, 42, 5, 50, 90, 100]
-# # # # i = 4, (current)
+def radixSort(inputlist):
 
- 
-n = len(mylist)
-print(mylist)
+    n = len(inputlist)
+    if n <= 1:
+        return inputlist
 
-for i in range(1, n):
-    current_value = mylist[i] 
-    index = i
+    max_value = max(inputlist) 
+    exp = 10
 
-    for j in range(i - 1, -1, -1):
-        print(mylist[j], mylist[i])
+    radixArray = []
 
-        if mylist[i] < mylist[j]: # if unsorted element is smaller than sorted element:
-            index = j
-            mylist[j+1] = mylist[j]
-    if index != i:
-        mylist[index] = current_value
+    while max_value//exp > 0:
+        for _ in range(n):
+            radixArray.append([])
 
-        """
-        if mylist[i] < mylist[j]: # if unsorted element is smaller than sorted element
-            pop_value = mylist.pop(i) # remove it from unsorted portion => elements after index i shifted to left by 1 
-            mylist.insert(j, pop_value) # insert it into the sorted portion => elements after index j shifted to right by 1
-            break
-        """
+        for val in inputlist:
+            index = val % exp
+            radixArray[index].append(val)
+        exp *= 10
 
-    print(mylist)
+    print(radixArray)
 
-
+radixSort(mylist)
