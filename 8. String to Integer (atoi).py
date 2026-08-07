9ae691b2d5a48d5b8e9d83dc2402c057ea0cd9fd
jiang93 <jianghy1993@hotmail.com>
2026-08-07 16:25:00 +0800

    Updated solution for Leet Code Exercise 8

--- 8. String to Integer (atoi).py
+++ 8. String to Integer (atoi).py
@@ -2,39 +2,43 @@
 # step 2: read + or - sign
 # step 3: read number until next charater is a non-digit, leading 0 is ignored
 # # cannot use int() function
+
 class Solution:
     def myAtoi(self, s: str) -> int:
-
-        s_num = str()
-        numbers = ["0", "1", "2", "3", "4", "5", "6", "7", "8", "9"]
+        string_num = 0
         sign = 1
-        if s[0].isspace: # remove leading space
-            s = s.lstrip() 
 
-        if s[0] == "-": # remove -/+ sign 
+        if not s:
+            return 0
+        
+        if s[0].isspace: # remove leading space
+            s = s.lstrip()
+
+        if s and s[0] == "-": # remove -/+ sign 
             sign = -1
-            s = s.lstrip("-")
+            s = s.removeprefix("-")
         else:
-             s = s.lstrip("+")
+             s = s.removeprefix("+")
 
         for i in s:
-            if i in numbers:
-                s = s.lstrip("0")
-                s_num += i
+            if i.isdigit():
+                # ord(i) converts the character into ascii code value
+                string_num = string_num*10 + (ord(i)-48)
+                # print(ord(i)) 
             else:
                 break
 
-        s_num = int(s_num)
-        num = sign*s_num
+        string_num *= sign
 
-        if num < -2**31:
-            num = -2**31
+        if string_num < -2**31:
+            string_num = -2**31
 
-        if num > 2**31-1:
-            num = 2**31-1
+        if string_num > 2**31-1:
+            string_num = 2**31-1
 
-        print(num)
-        return num
+        print(string_num)
+        return string_num
+
 
 string_num = Solution()
 string_num.myAtoi("1337c0d3")
